317537fc8da0d6f277eab15d30ecb2230b85135a
moretza rezaeian <morteza78rezaeian@gmail.com>
2022-05-15 20:04:31 +0430

    Project 1 Done!

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,4 +1,26 @@
-const AllEventPage = () => {
-  return <div>AllEventPage</div>;
-};
-export default AllEventPage;
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+
+function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    
+    router.push(fullPath);
+  }
+
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export default AllEventsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import EventList from '../components/EventList';
 import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
 
-const HomePage = () => {
+function HomePage() {
   const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
       <EventList items={featuredEvents} />
     </div>
   );
-};
+}
 
 export default HomePage;
